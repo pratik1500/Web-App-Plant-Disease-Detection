7a9373b83f8b0f4bdead41881832a827936f3b46
pratik1500 <pratik_mahankal@outlook.com>
2021-04-01 11:33:59 +0530

    initial commit

--- predict.py
+++ predict.py
@@ -3,4 +3,83 @@ import torch
 import torch.nn as nn 
 import torchvision.transforms as transforms 
 from PIL import Image
+from torchsummary import summary              # for getting the summary of our model
 
+
+disease = ['Tomato___Late_blight', 'Tomato___healthy', 'Grape___healthy', 'Orange___Haunglongbing_(Citrus_greening)', 'Soybean___healthy', 'Squash___Powdery_mildew', 'Potato___healthy', 'Corn_(maize)___Northern_Leaf_Blight', 'Tomato___Early_blight', 'Tomato___Septoria_leaf_spot', 'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Strawberry___Leaf_scorch', 'Peach___healthy', 'Apple___Apple_scab', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Bacterial_spot', 'Apple___Black_rot', 'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Peach___Bacterial_spot', 'Apple___Cedar_apple_rust', 'Tomato___Target_Spot', 'Pepper,_bell___healthy', 'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)', 'Potato___Late_blight', 'Tomato___Tomato_mosaic_virus', 'Strawberry___healthy', 'Apple___healthy', 'Grape___Black_rot', 'Potato___Early_blight', 'Cherry_(including_sour)___healthy', 'Corn_(maize)___Common_rust_', 'Grape___Esca_(Black_Measles)', 'Raspberry___healthy', 'Tomato___Leaf_Mold', 'Tomato___Spider_mites Two-spotted_spider_mite', 'Pepper,_bell___Bacterial_spot', 'Corn_(maize)___healthy']
+print(len(disease))
+
+# convolution block with BatchNormalization
+def ConvBlock(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+             nn.BatchNorm2d(out_channels),
+             nn.ReLU(inplace=True)]
+    if pool:
+        layers.append(nn.MaxPool2d(4))
+    return nn.Sequential(*layers)
+
+
+# resnet architecture 
+class ResNet9(nn.Module):
+    def __init__(self, in_channels, num_diseases):
+        super().__init__()
+        
+        self.conv1 = ConvBlock(in_channels, 64)
+        self.conv2 = ConvBlock(64, 128, pool=True) # out_dim : 128 x 64 x 64 
+        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
+        
+        self.conv3 = ConvBlock(128, 256, pool=True) # out_dim : 256 x 16 x 16
+        self.conv4 = ConvBlock(256, 512, pool=True) # out_dim : 512 x 4 x 44
+        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4),
+                                       nn.Flatten(),
+                                       nn.Linear(512, num_diseases))
+        
+    def forward(self, xb): # xb is the loaded batch
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out        
+
+
+model = ResNet9(3, 38)
+INPUT_SHAPE = (3, 256, 256)
+PATH = "plant-disease-model-complete.pth"
+
+device = torch.device("cpu")
+PATH = "plant-disease-model.pth"
+device = torch.device('cpu')
+# model = ResNet9(*args, **kwargs)
+model.load_state_dict(torch.load(PATH, map_location=device))
+
+
+
+# image -> tensor
+def transform_image(image_bytes):
+    transform = transforms.Compose([transforms.ToTensor()])
+
+    image = Image.open(io.BytesIO(image_bytes))
+    return transform(image).unsqueeze(0)
+
+def get_prediction(image_tensor):
+    # images = image_tensor.reshape(-1, 256*256)
+    outputs = model(image_tensor)
+        # max returns (value ,index)
+    _, predicted = torch.max(outputs.data, 1)
+    return disease[predicted]
+
+
+with open('0b37761a-de32-47ee-a3a4-e138b97ef542___JR_FrgE.S 2908_new30degFlipLR.JPG', 'rb') as f:
+    img_bytes = f.read()
+
+
+
+tensor = transform_image(img_bytes)
+predicted=get_prediction(tensor)
+
+print(predicted)
